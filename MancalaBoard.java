c7301777048ba8973eeabca863b81524e524d9c8
Solale5 <solomon.alemu@sjsu.edu>
2021-05-05 16:23:54 -0700

    added jeremys changes

--- MancalaBoard.java
+++ MancalaBoard.java
@@ -10,6 +10,7 @@ public class MancalaBoard {
     private boolean p1turn;
     private boolean p2turn;
     private boolean stonesMoved;
+    private boolean needsToConfirm;
 
     public MancalaBoard() {
         stones = new int[]{4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0};
@@ -42,13 +43,11 @@ public class MancalaBoard {
         stones = array;
     }
 
-    /**
-     * Perform a player's turn by automoving the stones
-     *
-     * @param slot the pit that a player picks from that maps to the array index
-     * @return whether the user's turn is ended
-     */
-    protected boolean moveStones(int slot) {
+    public boolean moveStones(int slot) {
+        if(needsToConfirm)
+        {
+            return true;
+        }
         stonesMoved = true;
         int pointer = slot;
         prevStones = stones.clone();
@@ -66,6 +65,7 @@ public class MancalaBoard {
         } else if (slot >= 7 && whichTurn() == 1) {
             return true;
         }
+
         // take stones out of pit
         int theStones = stones[slot];
         stones[slot] = 0;
@@ -75,20 +75,58 @@ public class MancalaBoard {
             if (pointer == 13) {
                 stones[pointer]++;
                 theStones--;
+                if(theStones == 0)
+                {
+                    break;
+                }
                 pointer = 0;
-                stones[pointer]++;
-                theStones--;
             } else {
                 stones[pointer]++;
                 theStones--;
             }
         }
+        //Condition if player ends turn in their mancala -> Go again
+        if((p1turn) && (pointer == 6))
+        {
+            l.stateChanged(new ChangeEvent(this));
+            return true;
+        }
+        else if((p2turn) && (pointer == 13))
+        {
+            l.stateChanged(new ChangeEvent(this));
+            return true;
+        }
+
+        //Condition if player ends on empty pit of their own -> capture enemy's stones across from pit
+        int oppositePit = 12 - pointer;
+        if(pointer!=6 && pointer != 13)
+        {
+            if((p1turn) && (stones[pointer] == 1) && (stones[oppositePit] != 0))
+            {
+                stones[6]+= stones[pointer] + stones[oppositePit];
+                stones[pointer] = 0;
+                stones[oppositePit] = 0;
+            }
+            else if((p2turn) && (stones[pointer] == 1) && (stones[oppositePit] != 0))
+            {
+                stones[13] += stones[pointer] + stones[oppositePit];
+                stones[pointer] = 0;
+                stones[oppositePit] = 0;
+            }
+        }
+
         l.stateChanged(new ChangeEvent(this));
+        needsToConfirm = true;
         return true;
     }
 
     public boolean stonesMoved() {
         return stonesMoved;
+    }
+
+    public void setNeedsToConfirm(boolean x)
+    {
+        needsToConfirm = x;
     }
 
     public void switchTurn() {
